63e222575515bbc9e4fe05647fdceca5f24a8570
Siddharth Kundu <sk31395@gmail.com>
2019-09-26 23:39:55 +0530

    Dynamically add ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,24 @@ import React from 'react'
 import './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
-const Burger = () => {
+const Burger = ({ ingredients }) => {
+	let transformedIngredients = Object.keys(ingredients)
+		.map(igKey => {
+			return [...Array(ingredients[igKey])].map((_, i) => {
+				return <BurgerIngredient key={igKey + i} type={igKey} />
+			})
+		})
+		.reduce((arr, el) => {
+			return arr.concat(el)
+		}, [])
+
+	if (transformedIngredients.length === 0)
+		transformedIngredients = <p>Please start adding ingredients</p>
+
 	return (
 		<div className='Burger'>
 			<BurgerIngredient type='bread-top' />
-			<BurgerIngredient type='cheese' />
-			<BurgerIngredient type='meat' />
+			{transformedIngredients}
 			<BurgerIngredient type='bread-bottom' />
 		</div>
 	)
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,19 @@ import Auxx from '../../hoc/Auxx'
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+	state = {
+		ingredients: {
+			salad: 0,
+			bacon: 0,
+			cheese: 0,
+			meat: 0
+		}
+	}
+
 	render() {
 		return (
 			<Auxx>
-				<Burger />
+				<Burger ingredients={this.state.ingredients} />
 				<div>Build Controls</div>
 			</Auxx>
 		)
