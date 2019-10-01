1ea127aee88ed7511036fcdc85124f857f627016
Siddharth Kundu <sk31395@gmail.com>
2019-10-01 23:54:47 +0530

    Initial toolbar component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Aux from '../../hoc/Auxx'
 import './Layout.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout = props => (
 	<Aux>
-		<div>Toolbar, Sidebar, Backdrop</div>
+		<Toolbar />
 		<main className='Content'>{props.children}</main>
 	</Aux>
 )
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Toolbar.css'
+
+const Toolbar = () => (
+	<header className='Toolbar'>
+		<div>MENU</div>
+		<div>LOGO</div>
+		<nav>
+			{/* <ul></ul> */}
+			...
+		</nav>
+	</header>
+)
+
+export default Toolbar
